31e874488269e65abdbe12fefbe61f59ecf36ffe
vmadmin <vmadmin@DevRGVM>
2019-05-26 06:57:47 +0000

    fb2 New Application

---
+++ DemApplication/NewApplication1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewApplication1.Startup))]
+namespace NewApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
